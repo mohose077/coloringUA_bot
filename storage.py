092162ba133053b6144f47a66459f1d31cd95cbd
mohose077 <mail.work071@gmail.com>
2025-04-11 17:49:02 +0300

    Create storage.py

---
+++ storage.py
@@ -0,0 +1,5 @@
+def log_user_choice(user_id, choices):
+    print(f"USER {user_id} вибрав: {choices}")
+
+def log_user_feedback(user_id, image_url, feedback):
+    print(f"USER {user_id} надав оцінку {feedback} для {image_url}")
