cc9d2245545c416ea192d4e4c63f52e81f8f41ca
mohose077 <mail.work071@gmail.com>
2025-04-11 17:49:21 +0300

    Create generator.py

---
+++ generator.py
@@ -0,0 +1,4 @@
+def generate_coloring_image(age, theme, format):
+    # Виклик API Replicate або іншого генератора
+    # Повертає URL або шлях до зображення
+    return "https://example.com/image.png"
